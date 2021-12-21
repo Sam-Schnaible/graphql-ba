c82f18a312df6fa42f560dbf46c377072b969ac0
Sam Schnaible II <sschnaible83@gmail.com>
2021-12-20 18:10:41 -0800

    set mutation args non nullable

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,7 +9,8 @@ const {
     GraphQLInt,
     GraphQLID,
     GraphQLSchema,
-    GraphQLList
+    GraphQLList,
+    GraphQLNonNull
     } = graphql;
 
 
@@ -82,8 +83,8 @@ const Mutation = new GraphQLObjectType({
         addAuthor: {
             type: AuthorType,
             args: {
-                name: {type: GraphQLString},
-                age: {type: GraphQLInt}
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                age: {type: new GraphQLNonNull(GraphQLInt)}
             },
             resolve(parent, args) {
                 let author = new Author({
@@ -96,8 +97,8 @@ const Mutation = new GraphQLObjectType({
         addBook: {
             type: BookType,
             args: {
-                name: { type: GraphQLString},
-                genre: { type: GraphQLString}
+                name: { type: new GraphQLNonNull(GraphQLString)},
+                genre: { type: new GraphQLNonNull(GraphQLString)}
             },
             resolve(parent, args) {
                 let book = new Book({
