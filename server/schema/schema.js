f083bb38aa1227c31f3b5ec9f17eaa7009bce621
Sam Schnaible II <sschnaible83@gmail.com>
2021-12-20 17:56:01 -0800

    return author for BookType from database

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -16,15 +16,14 @@ const {
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
-    fields: ( ) => ({
+    fields: () => ({
         id: { type: GraphQLString },
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
         author: {
             type: AuthorType,
             resolve(parent, args) {
-                console.log(parent);
-                // return _.find(authors, {id: parent.authorId});
+                return Author.findById(parent.authorId);
             }
         }
     })
