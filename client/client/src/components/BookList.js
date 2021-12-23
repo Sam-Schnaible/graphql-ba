d0cefdec473398fad74235c55d7c8dee22f33a49
Sam Schnaible II <sschnaible83@gmail.com>
2021-12-23 11:02:40 -0800

    write query to retrieve book data

--- client/client/src/components/BookList.js
+++ client/client/src/components/BookList.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
 
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`
 
 const BookList = () => {
+  const { loading, error, data } = useQuery(getBooksQuery);
+
+  if ( loading ) return 'Loading...';
+  if ( error ) return  `Error! ${error.message}`;
+  console.log(data);
   return (
-    <div id="main">
+    <div>
       <ul id="book-list">
+
         <li>Book name</li>
       </ul>
     </div>
