6fa2284ff211dae132076cbe10c9279e25522a04
Sam Schnaible II <sschnaible83@gmail.com>
2021-12-22 16:32:45 -0800

    initialize and connect client to react

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,12 +1,27 @@
 import BookList from './components/BookList.js';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
+
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div id="main">
-      <header className="App-header">
+      <header className="GraphQL">
+      <h1>Sam's Book List</h1>
       <BookList />
       </header>
     </div>
+    </ApolloProvider>
   );
 }
 
--- client/client/src/components/BookList.js
+++ client/client/src/components/BookList.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
+
 const BookList = () => {
   return (
     <div id="main">
-      <h1>Sam's Book List</h1>
       <ul id="book-list">
         <li>Book name</li>
       </ul>
