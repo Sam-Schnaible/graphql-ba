14edc63660c934bdc8934ec01e683e44db6a48fd
Sam Schnaible II <sschnaible83@gmail.com>
2021-12-28 18:13:10 -0800

    add component BookDetails

---
+++ client/client/src/components/BookDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+// import { getBooksQuery } from '../queries/queries.js';
+
+const BookDetails = () => {
+
+  return (
+    <div id="book-details">
+      <p>Output book details here</p>
+    </div>
+  )
+}
